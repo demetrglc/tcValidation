b97b6bbe83a9948e3749b20c4e477991e524a6f1
mustafayildiz12 <hasansabbah284@gmail.com>
2022-10-02 21:11:36 +0300

    fix response messages

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class TcValidation extends StatefulWidget {
+  const TcValidation({Key? key}) : super(key: key);
+
   @override
   State<TcValidation> createState() => _TcValidationState();
 }
@@ -16,26 +18,30 @@ class _TcValidationState extends State<TcValidation> {
 
   TextEditingController tc1 = TextEditingController();
 
-  tcDogrula() {
-    sayi = tc1.text;
+  tcDogrula(String myNumber) {
     for (int i = 0; i < 9; i = i + 2) {
-      sonucTekToplam = sonucTekToplam! + int.parse(charAt(sayi, i));
+      sonucTekToplam = sonucTekToplam! + int.parse(charAt(myNumber, i));
     }
     sonucTekCarpma = sonucTekToplam! * 7;
 
     for (int j = 1; j < 9; j = j + 2) {
-      sonucCiftToplam = sonucCiftToplam! + int.parse(charAt(sayi, j));
+      sonucCiftToplam = sonucCiftToplam! + int.parse(charAt(myNumber, j));
     }
 
     onuncuHane = (sonucTekCarpma! - sonucCiftToplam!) % 10;
 
     onbirinciHane = (sonucTekToplam! + sonucCiftToplam! + onuncuHane!) % 10;
 
-    if (int.parse(charAt(sayi, 9)) == onuncuHane &&
-        int.parse(charAt(sayi, 10)) == onbirinciHane) {
-      Text("bu doğru bir tc");
-    } else
-      Text("yanlış tc");
+    if (int.parse(charAt(myNumber, 9)) == onuncuHane &&
+        int.parse(charAt(myNumber, 10)) == onbirinciHane) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('True Tc'),
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('False Tc'),
+      ));
+    }
   }
 
   String charAt(sayi, int position) {
@@ -45,9 +51,9 @@ class _TcValidationState extends State<TcValidation> {
       return '';
     }
 
-    int _realPosition = position < 0 ? sayi.length + position : position;
+    int realPosition = position < 0 ? sayi.length + position : position;
 
-    return sayi[_realPosition];
+    return sayi[realPosition];
   }
 
   @override
@@ -56,13 +62,24 @@ class _TcValidationState extends State<TcValidation> {
       body: Center(
           child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 55,
           ),
           TextField(
             controller: tc1,
+            keyboardType: TextInputType.number,
           ),
-          ElevatedButton(onPressed: tcDogrula, child: Text("Tc Doğrula")),
+          ElevatedButton(
+              onPressed: () {
+                if (tc1.text.length != 11) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('Empty Tc'),
+                  ));
+                } else {
+                  tcDogrula(tc1.text);
+                }
+              },
+              child: const Text("Tc Doğrula")),
         ],
       )),
     );
